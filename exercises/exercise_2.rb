dc334767df7af39b709714135dbe962a8dd9e898
sddanila <sddanila@yahoo.co.uk>
2019-03-19 12:26:10 +0000

    add solution to 2nd exercise with changing first store's name to Burnabee and deleting 3rd store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store3 = Store.find_by(id: 3)
+
+@store1.update(name: 'Burnabee')
+@store3.destroy
+puts @store1.name
+puts @store2.name
+puts Store.all.count
